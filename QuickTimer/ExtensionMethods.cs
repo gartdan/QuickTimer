b86f69a1a0b26207f546a7dad1193ba47b80355e
Dan Gartner <dgartner@gmail.com>
2016-02-01 11:35:02 -0600

    Changed from L to d

--- QuickTimer/ExtensionMethods.cs
+++ QuickTimer/ExtensionMethods.cs
@@ -4,11 +4,11 @@
     {
         public static string ToSeconds(this long milliseconds)
         {
-            return $"{milliseconds / 1000L} seconds.";
+            return $"{milliseconds / 1000d} seconds.";
         } 
 
         //4. Lambda body extenstion methods
-        //public static string ToSeconds(this long milliseconds) => $"{milliseconds / 1000L} seconds.";
+        //public static string ToSeconds(this long milliseconds) => $"{milliseconds / 1000d} seconds.";
 
     }
 }
