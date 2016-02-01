6b1e3cb3d1b4b793177f9513cbc92d990c233328
Dan Gartner <dgartner@gmail.com>
2016-02-01 11:30:13 -0600

    Lambda body extension method

--- QuickTimer/ExtensionMethods.cs
+++ QuickTimer/ExtensionMethods.cs
@@ -4,7 +4,11 @@
     {
         public static string ToSeconds(this long milliseconds)
         {
-            return $"{milliseconds / 1000d} seconds.";
-        }
+            return $"{milliseconds / 1000L} seconds.";
+        } 
+
+        //4. Lambda body extenstion methods
+        //public static string ToSeconds(this long milliseconds) => $"{milliseconds / 1000L} seconds.";
+
     }
 }
